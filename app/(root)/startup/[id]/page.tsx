9a90626589c92f006dde13889aec3301775250ad
Yogendra Bhadouriya <yogendrabhadouriya@Yogendras-MacBook-Pro.local>
2024-12-09 12:32:02 +0530

    lint fix

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -18,10 +18,14 @@ export const experimental_ppr = true;
 const Page = async ({ params } : {params : Promise<{ id: string}>}) => {
     const id = (await params).id;
 
-    const [post, { select: editorPosts}] = await Promise.all([
-        client.fetch(STARTUP_BY_ID_QUERY, {id}),
-        client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'editor-pics'})
-    ]);
+    // const [post, { select: editorPosts}] = await Promise.all([
+    //     client.fetch(STARTUP_BY_ID_QUERY, {id}),
+    //     client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'editor-pics'})
+    // ]);
+
+    const post = await client.fetch(STARTUP_BY_ID_QUERY, {id});
+
+    // const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'editor-pics'});
 
     if (!post) return notFound();
 
@@ -80,16 +84,16 @@ const Page = async ({ params } : {params : Promise<{ id: string}>}) => {
 
                 <hr className="divider" />
 
-                {editorPosts?.length > 0 && (
-                    <div className="max-w-4xl mx-auto">
-                        <p className="text-30-semibold">Editor Pics</p>
-                        <ul className="mt-7 card_grid-sm">
-                            {editorPosts.map((post: StartupTypeCard, index: number) => (
-                                <StartupCard post={post} key={index} />
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                {/*{editorPosts?.length > 0 && (*/}
+                {/*    <div className="max-w-4xl mx-auto">*/}
+                {/*        <p className="text-30-semibold">Editor Pics</p>*/}
+                {/*        <ul className="mt-7 card_grid-sm">*/}
+                {/*            {editorPosts.map((post: StartupTypeCard, index: number) => (*/}
+                {/*                <StartupCard post={post} key={index} />*/}
+                {/*            ))}*/}
+                {/*        </ul>*/}
+                {/*    </div>*/}
+                {/*)}*/}
 
                 <Suspense fallback={<Skeleton className="view_skeleton" />}>
                     <View id={id} />
